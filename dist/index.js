371e517574abafda8b4159bd586abf57aae6836f
Higa <codehiga@gmail.com>
2022-07-06 21:48:25 -0300

    nest project config

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = 8080;
-app.get("/", (req, res) => {
-    res.send("API Online!");
-});
-app.listen(PORT, () => {
-    console.log("ON!");
-});
